1245274af14a10c78800d7005287ece087d19894
Salman Tanvir <salmantanvirshuvo@gmail.com>
2026-09-10 20:21:32 +0600

    Added Star Reverse printing

--- Star print reverse.c
+++ Star print reverse.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main (){
 
-int n , star=1;
+int n ;
 scanf ("%d",&n);
+int star = n;
 for (int i=1;i<=n;i++){
     for (int j=1;j<=star;j++){
         printf ("*");
        
     }
      printf ("\n");
-     star++;
+     star--;
 }
 
 
