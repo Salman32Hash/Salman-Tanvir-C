092a74970769d9192432f4e0f6c6a6e5523bbcbf
Salman Tanvir <salmantanvirshuvo@gmail.com>
2026-09-15 20:48:42 +0600

    Adeed C problems

---
+++ sumof 2 values equal x.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int x ;
+    scanf("%d", &x);
+    for (int i = 0; i < n; i++) {
+       for (int j = i + 1; j < n; j++) {
+            if (a[i] + a[j] == x) {
+                printf("%d %d\n", a[i], a[j]);
+            }
+        }
+    }
+    return 0;
+}
